6ed905fa1c1eb89fe618e632f680d529d769c165
Archit5655 <architgargagra@gmail.com>
2024-10-01 12:57:38 +0530

    made some chamnges

--- db/db.js
+++ db/db.js
@@ -5,8 +5,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI, {
+    console.log();
+    await mongoose.connect("mongodb://localhost:27017/voyagai", {
     });
     console.log('MongoDB connected');
   } catch (err) {
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/wishlist', wishlist);
 app.use('/', AIRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
